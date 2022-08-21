16f0c463318f606d6f645ebefef8f4a03c394220
GodfredAsa <goldenheroe1@yahoo.com>
2022-08-21 22:20:24 +0000

    implements message logout and user input wrong

--- portfolio/users/views.py
+++ portfolio/users/views.py
@@ -1,8 +1,36 @@
-from pydoc import describe
+
 from unicodedata import name
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import  authenticate, login,logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+
 from .models import Profile
 
+def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect('profiles')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        try:
+            user = User.objects.get(username = username)
+        except:
+            messages.error(request, 'User does not exist ')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'username OR password is incorrect')
+    return render(request, 'users/login_register.html')
+
+def logoutUser(request):
+    logout(request)
+    messages.error(request, 'User logged out ')
+    return redirect('login')
 
 def profiles(request):
     profiles = Profile.objects.all()
@@ -15,4 +43,5 @@ def user_profile(request, pk):
     skills = profile.skill_set.filter() # profile skill without description 
     context = {'profile': profile, 'skills': skills}
     return render(request, 'users/user-profile.html', context)
-    
+
+
